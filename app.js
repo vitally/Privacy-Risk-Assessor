40a654adc9c13cfee7b38cb31e0171262beccdc3
Vitally Tezhe <vitally@tezhe.eu>
2022-11-12 13:22:26 +0200

    database related changes

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 import  http from 'http';
-import { getMostPopularSitesSet } from './modules/sites/mostpopularsites.js'
-
+import { getMostPopularSitesSet } from './modules/sites/mostPopularSites.js';
+import { DatabaseHelper } from './modules/database/databaseHelper.js';
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const mongoURI = 'mongodb://127.0.0.1:27017/';
+
+const sites = [];
+
+const database = new DatabaseHelper(mongoURI);
+await database.initializeConnectionAndOpenDatabase('latvianTrackers');
+
+getMostPopularSitesSet('https://trends.netcraft.com/topsites?c=LV')
+  .then( popularSiteSet => {
+    for (const popularSite in popularSiteSet) {
+      if (Object.hasOwnProperty.call(popularSiteSet, popularSite)) {
+        const siteURL = popularSiteSet[popularSite];
+        const siteObject = {
+          domainAddress : siteURL,
+          visitDate : new Date(),
+          scheme: siteURL.substring(0,siteURL.indexOf(':')),
+          fullAddress: ''
+        };
+        sites.push[siteObject];
+      }
+    }
+  })
+  .catch( err => {
+    console.error(err);
+  });
+
+//mostPopularLatvianSites
+
+
+await database.listDatabases();
+await database.closeConnection();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
